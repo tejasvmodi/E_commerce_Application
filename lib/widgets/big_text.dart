327954b89848638f4a346a6754a257423d2fc522
tejasvmodi <moditejasv@gmail.com>
2023-11-09 16:44:15 +0530

    done

--- lib/widgets/big_text.dart
+++ lib/widgets/big_text.dart
@@ -1,6 +1,7 @@
 import 'package:e_commerce_application/utils/dimension.dart';
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class BigText extends StatelessWidget {
    Color? color;
   final String text;
